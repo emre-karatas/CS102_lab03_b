5ea347f1b9b66f91f032676d734e93ab2ca68fcd
emrekaratas22001641 <emrekaratas22001641@gmail.com>
2021-07-01 17:26:30 +0300

    Int Iterator interface is done

---
+++ IntIterator.java
@@ -0,0 +1,11 @@
+import java.util.Iterator;
+
+/**
+ * This is a int Iterator
+ * @author Emre Karatas-22001641
+ * @version v1.0 - 30.06.2021
+ */
+public interface IntIterator
+{
+    int nextInt();
+}
